00e8860816ae953551e5df4a55cbe737b5e086cd
yiren.chow@gmail.com <yiren.chow@gmail.com>
2021-05-25 18:48:36 +0800

    add testing

--- rskernel/src/main.rs
+++ rskernel/src/main.rs
@@ -1,32 +1,34 @@
-#![no_std] // don't link the Rust standard library
-#![no_main] // disable all Rust-level entry points
+// in src/main.rs
+
+#![no_std]
+#![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(rskernel::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
+use rskernel::println;
 
-mod vga_buffer;
-
-static HELLO: &[u8] = b"Hello World!";
-
-// The main function doesn't make sense without
-// an underlying runtime that calls it.
-// #[no_mangle] attribute disables the name mangling
-// to ensure that the Rust compiler outputs
-// a function with the name _start, instead of generating
-// some cryptic symbol to give every function an unique name.
-// This is required because we need to tell the name of the
-// entry point function to the linker.
-// ! return type means that the function is diverging, i.e. not
-// allowed to ever return.
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
 
+    #[cfg(test)]
+    test_main();
+
     loop {}
 }
 
-// This function is called on panic.
+/// This function is called on panic.
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
     loop {}
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    rskernel::test_panic_handler(info)
 }
--- rskernel/src/vga_buffer.rs
+++ rskernel/src/vga_buffer.rs
@@ -151,3 +151,18 @@ pub fn _print(args: fmt::Arguments) {
     use core::fmt::Write;
     WRITER.lock().write_fmt(args).unwrap();
 }
+
+/*
+ * Unit tests.
+*/
+
+// Test function to verify that the printed lines really appear on the screen.
+#[test_case]
+fn test_println_output() {
+    let s = "Some test string that fits on a single line";
+    println!("{}", s);
+    for (i, c) in s.chars().enumerate() {
+        let screen_char = WRITER.lock().buffer.chars[BUFFER_HEIGHT - 2][i].read();
+        assert_eq!(char::from(screen_char.ascii_character), c);
+    }
+}
